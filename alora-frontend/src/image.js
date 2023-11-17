7de17d9035cf78812af8f24dd5ade400493f56ca
jfrauenhofer <jackfrauenhofer@gmail.com>
2023-11-16 22:34:22 -0500

    Layout and pictures

---
+++ alora-frontend/src/image.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import img1 from './images/Sprout.png';
+import img2 from './images/Home-Dude.png'
+
+
+function Images(){
+
+    return(
+        <div>
+            <img src={img1} alt=""/>
+        </div>
+    )
+}
+
+export default Images
