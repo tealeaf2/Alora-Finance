0f4867ac68ae139f37cc9538ecf456c00cb117c4
Charles0208 <cjiang0208@icloud.com>
2023-11-25 14:01:59 -0500

    initial commit

---
+++ alora-frontend/src/global/header.js
@@ -0,0 +1,39 @@
+// sys.path.append('../src')
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '..index';
+import App from '..App';
+import reportWebVitals from '..reportWebVitals';
+
+
+// .button {
+//     background-color: #04AA6D; /* Green */
+//     border: none;
+//     color: white;
+//     padding: 15px 32px;
+//     text-align: center;
+//     text-decoration: none;
+//     display: inline-block;
+//     margin: 4px 2px;
+//     cursor: pointer;
+//   }
+
+<div className='App-header'>
+    <div className='App-logo'>
+        <a className='add-btn'>LOGO</a>
+    </div>
+    <div className='add-section'>
+        <a className='add-btn' href='#'>lessons</a>
+    </div>
+    <div className='add-section'>
+        <a className='add-btn' href='#'>games</a>
+    </div>
+    <div className='progress'>
+        <a className='add-btn' href='#'>progress</a>
+    </div>
+    <div className='profile'>
+        <a className='add-btn' href='#'>profile</a>
+    </div>
+
+</div>
