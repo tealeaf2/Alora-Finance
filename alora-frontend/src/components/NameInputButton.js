68670d8f2f1f6765e9e393c1122841ef70dd9a42
lucawatson <lucawwatson@gmail.com>
2024-01-15 20:32:23 -0800

    added submit button functionality

--- alora-frontend/src/components/NameInputButton.js
+++ alora-frontend/src/components/NameInputButton.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 
-//function to store name from input box
-const getValue = (e) => {
-    const uName = e.target.value;
-    console.log(uName);
-}
-
  const NameInputButton = ()  => {
+
+    const [currentVal, setVal] = useState(' ')
+    
+    //function to store name from input box
+    const getValue = (e) => {
+        setVal(e.target.value);
+    }
+    
+    //function to output name (as a test, will be sent to django api later)
+    const handleClick = () => {
+        console.log(currentVal)
+    }
+    
+
     return (
+
     <>
         <label for="Name">Enter your name: </label>
         <input type="text" 
         name = "fname"
         placeholder = "Your name"
-        onChange =  { getValue } />
+        onChange = { getValue }
+        />
+        <button type = 'submit' onClick = { handleClick }> Submit </button>
     </>
     )
 }
