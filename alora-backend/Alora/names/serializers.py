a93a94025bdd3a3d71a6b586c58b9fc5bcc11a6f
tealeaf2 <shadowrocket2003@gmail.com>
2024-01-16 23:20:41 -0500

    removing alora app

--- alora-backend/Alora/names/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Name
-
-class NameSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Name
-        fields = ['id', 'name']
