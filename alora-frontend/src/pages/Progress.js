5abebe5abda4323806bde127e9357ebe3872b2bc
Jack <jcm2022@icloud.com>
2024-01-17 23:19:30 -0500

    Ad is in lower left corner, rn it uses random image API. Wasn't displaying image before, fixed by making a more direct path (defining ad directly rather than through the JSON array)

--- alora-frontend/src/pages/Progress.js
+++ alora-frontend/src/pages/Progress.js
@@ -3,6 +3,7 @@ import sprout from '../images/Sprout.png';
 import home_dude from '../images/Home-Dude.png';
 import money_tree from'../images/Money_Tree.png';
 import Header from '../global/Header';
+import Adspace from '../global/Adspace'
 
 function Progress() {
     return (
@@ -12,7 +13,11 @@ function Progress() {
           <img src={sprout} className="sprout" alt="" width="35%" height="auto"/>
           <img src={money_tree} className="money_tree" alt="" width="20%" height="auto"/>
           <div className="horizontal-line"></div>
+
+          <Adspace/>
+          {/* <img src={'https://random.imagecdn.app/500/150'}/> */}
         </div>
+
       )
 }
 
