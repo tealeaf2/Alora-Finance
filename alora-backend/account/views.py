f7b5ba1857d69ed729c4dbba00faf2d6fdc1f337
Kyae <johnkimdw@gmail.com>
2024-01-28 19:28:28 -0500

    add jwt token

--- alora-backend/account/views.py
+++ alora-backend/account/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 # Create your views here.
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+
+        return data
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
