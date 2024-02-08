1e77d75252c43abfea0ff8487eddcf89fb973078
Ethan Little <elittle2@nd.edu>
2024-02-07 22:33:44 -0500

    Changed scaling

--- alora-frontend/src/global/Header.js
+++ alora-frontend/src/global/Header.js
@@ -1,52 +1,79 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "../styles/index.css";
 import "../styles/App.css";
 
 import home_logo from "../images/Home_Logo.png";
 
 const Header = () => {
+  const location = useLocation();
+
   return (
     <header className="App-header border-b-4 border-gray-500">
-      <div className="max-w-screen-x1 sm:px-6 lg:px-8">
-        <div className="flex h-20 items-center  justify-start">
+      <div className="max-w-screen-x1">
+        <div className="flex h-20 items-center justify-start">
+          {/*Alora logo*/}
           <div className="App-header-home flex-1 md:flex md:items-center md:gap-5">
             <Link to="/progress">
               <img
                 alt="alora finance logo"
                 src={home_logo}
-                className="App-header-logo-width"
                 width="260px"
                 height="60px"
               />
             </Link>
           </div>
 
-          <div className="App-header-sections md:flex md:items-center md:mx-12">
-            <Link to="/lessons">
-              <div className="hidden sm:flex App-header-sections-button App-header-sections-button1">
-                Lessons
-              </div>
-            </Link>
+          {/*Page links*/}
+          <ul className="App-header-sections md:flex md:items-center md:mx-12">
+            <li>
+              <Link to="/lessons">
+                <button
+                  className={`sm:flex App-header-sections-button App-header-sections-button1 ${
+                    location.pathname === "/lessons" ? "button1-active" : ""
+                  }`}
+                >
+                  Lessons
+                </button>
+              </Link>
+            </li>
 
-            <Link to="/games">
-              <div className="hidden sm:flex App-header-sections-button App-header-sections-button2">
-                Games
-              </div>
-            </Link>
+            <li>
+              <Link to="/games">
+                <button
+                  className={`sm:flex App-header-sections-button App-header-sections-button2 ${
+                    location.pathname === "/games" ? "button2-active" : ""
+                  }`}
+                >
+                  Games
+                </button>
+              </Link>
+            </li>
 
-            <Link to="/progress">
-              <div className="hidden sm:flex App-header-sections-button App-header-sections-button3">
-                Progress
-              </div>
-            </Link>
+            <li>
+              <Link to="/progress">
+                <button
+                  className={`sm:flex App-header-sections-button App-header-sections-button3 ${
+                    location.pathname === "/progress" ? "button3-active" : ""
+                  }`}
+                >
+                  Progress
+                </button>
+              </Link>
+            </li>
 
-            <Link to="/profile">
-              <div className="hidden sm:flex App-header-sections-button App-header-sections-button4">
-                Profile
-              </div>
-            </Link>
-          </div>
+            <li>
+              <Link to="/profile">
+                <button
+                  className={`sm:flex App-header-sections-button App-header-sections-button4 ${
+                    location.pathname === "/profile" ? "button4-active" : ""
+                  }`}
+                >
+                  Profile
+                </button>
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     </header>
