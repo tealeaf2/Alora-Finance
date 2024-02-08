7bea088e39b7e7a2ab4abc5b95bd37e8fd768427
Ethan Little <elittle2@nd.edu>
2024-02-07 22:07:24 -0500

    Current page indicator and improved readability

--- alora-backend/backend/settings.py
+++ alora-backend/backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-##Hello Ethan was here
+
 from pathlib import Path
 from datetime import timedelta
 
